edfd43cc4c470c9230763d85539c948daa0caa17
AdamIzdebski <adam.izdebski1@gmail.com>
2023-12-06 13:37:00 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Hybrid Transformer',
+    version='0.0',
+    author='Adam Izdebski',
+    packages=find_packages(),
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    install_requires=[],
+    zip_safe=False)
