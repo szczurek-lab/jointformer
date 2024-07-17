34b30271d32412fc24b690e7c517f4ca5751444b
AdamIzdebski <adam.izdebski1@gmail.com>
2024-07-17 11:08:23 +0200

    modify architecture

--- jointformer/models/layers/mlp.py
+++ jointformer/models/layers/mlp.py
@@ -1,4 +1,6 @@
+import torch
 import torch.nn as nn
+from torch.nn import functional as F
 
 
 class MLP(nn.Module):
@@ -16,3 +18,22 @@ class MLP(nn.Module):
         x = self.proj(x)
         x = self.dropout(x)
         return x
+
+
+class FeedForward(nn.Module):
+
+    def __init__(self, embed_dim, bias, dropout):
+        super().__init__()
+        intermediate_size = find_multiple(int(8 * embed_dim / 3), 256)
+        self.w1 = nn.Linear(embed_dim, intermediate_size, bias=False)
+        self.w3 = nn.Linear(embed_dim, intermediate_size, bias=False)
+        self.w2 = nn.Linear(intermediate_size, embed_dim, bias=False)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.w2(F.silu(self.w1(x)) * self.w3(x))
+    
+
+def find_multiple(n: int, k: int) -> int:
+    if n % k == 0:
+        return n
+    return n + k - (n % k)
