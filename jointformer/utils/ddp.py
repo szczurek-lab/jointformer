2a410dc9ebb6961e84e78ba4eacdb9b5274ca3e9
AdamIzdebski <adam.izdebski1@gmail.com>
2024-07-25 17:32:36 +0200

    enable joint training

--- jointformer/utils/ddp.py
+++ jointformer/utils/ddp.py
@@ -10,10 +10,9 @@ DDP_BACKEND = "nccl"
 
 def init_ddp(enable_ddp: bool, rank: Optional[int] = None, world_size: Optional[int] = None) -> None:
 
-    rank = rank if rank is not None else int(os.environ["SLURM_PROCID"])
-    world_size = world_size if world_size is not None else int(os.environ["WORLD_SIZE"])
-
     if enable_ddp and int(os.environ.get('RANK', -1)) != -1:
+        rank = rank if rank is not None else int(os.environ["SLURM_PROCID"])
+        world_size = world_size if world_size is not None else int(os.environ["WORLD_SIZE"])
         init_process_group(backend=DDP_BACKEND, rank=rank, world_size=world_size)
         console.info("DDP initialized")
         
