c3c86cd2f13106c787a8098f3c4bffd6c397dfed
AdamIzdebski <adam.izdebski1@gmail.com>
2024-07-26 10:39:40 +0200

    add guacamol mpo target

---
+++ jointformer/utils/properties/smiles/guacamol_mpo.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+from guacamol import standard_benchmarks
+from typing import Optional
+
+from jointformer.utils.properties.smiles.base import BaseTarget
+
+GUACAMOL_MPO_DEFAULT_DTYPE = np.float64
+
+GUACAMOL_MPO_TASK_FN = {
+    'amlodipine': standard_benchmarks.amlodipine_rings(),
+    'fexofenadine': standard_benchmarks.hard_fexofenadine(),
+    'osimertinib': standard_benchmarks.hard_osimertinib(),
+    'perindopril': standard_benchmarks.perindopril_rings(),
+    'sitagliptin': standard_benchmarks.sitagliptin_replacement(),
+    'ranolazine': standard_benchmarks.ranolazine_mpo(),
+    'zaleplon': standard_benchmarks.zaleplon_with_other_formula(),
+}
+
+
+class GuacamolMPO(BaseTarget):
+    """ Guacamol MPO targets.
+    Source: 
+    """
+    
+    def _get_target(self, example: str, dtype: Optional[np.dtype] = GUACAMOL_MPO_DEFAULT_DTYPE) -> float:
+        try: 
+            target = np.array(list(map(lambda fn: fn.objective.score(example), GUACAMOL_MPO_TASK_FN.values())), dtype=dtype)
+            target[target < 0] = np.nan
+            return target
+        except Exception:
+            return np.full(len(GUACAMOL_MPO_TASK_FN), np.nan)
+
+    @property
+    def target_names(self):
+        return list(GUACAMOL_MPO_TASK_FN.keys())
+
+    def __repr__(self):
+        return "GuacamolMPO"
+
+    def __str__(self):
+        return "GuacamolMPO"
