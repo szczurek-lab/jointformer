f8ada0a94bf63485bccab2c0f832884ebcbc2678
AdamIzdebski <adam.izdebski1@gmail.com>
2024-07-26 12:54:21 +0200

    add 'exp' base to perplexity

--- jointformer/utils/metrics/perplexity.py
+++ jointformer/utils/metrics/perplexity.py
@@ -1,7 +1,7 @@
 import torch
 
 from torch.nn import functional as F
-from typing import Optional, Any
+from typing import Optional, Any, Optional, Union
 
 from jointformer.utils.metrics.base import BaseMetric
 
@@ -11,23 +11,28 @@ class Perplexity(BaseMetric):
     def __init__(self, reduction: Optional[str] = 'mean'):
         super().__init__(reduction=reduction, ignore_index=None)
 
-    def __call__(self, input: torch.Tensor, mask: torch.Tensor) -> float:
+    def __call__(self, input: torch.Tensor, mask: torch.Tensor, base: Optional[Union[float, str]] = 'exp') -> float:
         """ Calculates the perplexity of a sequence.
         
         Args:
             input: Logits of the sequence of shape (batch_size, seq_len, vocab_size).
             mask: Attention mask of the sequence of shape (batch_size, seq_len). Can pass None if no mask is required.
-            reduction: 'mean' or 'sum' or 'none'.
+            base: Base of the exponentiation used to calculate the perplexity. Can be 'exp' or a float or int.
         """
         if mask is not None:
             input = self._map_masked_logits_to_ignore_value(input, mask, float('nan'))
-        perplexity = self._calculate_perplexity(input)
+        perplexity = self._calculate_perplexity(input, base)
         return self._reduce(perplexity)
             
     @staticmethod
-    def _calculate_perplexity(logits: torch.Tensor, ignore_index: Optional[int] = None) -> torch.Tensor:
+    def _calculate_perplexity(logits: torch.Tensor, base: Union[float, str]) -> torch.Tensor:
             """ Calculates the perplexity of a sequence according to: https://github.com/ETHmodlab/CLM_perplexity/blob/main/src/python/helper.py
             """
             log_probs = F.log_softmax(logits, dim=-1).max(dim=-1).values
-            perplexity = 2 ** (-log_probs.nanmean(dim=-1))
-            return perplexity
+            if base == 'exp':
+                return torch.exp(-log_probs.nanmean(dim=-1))
+            elif isinstance(base, float) or isinstance(base, int):
+                return base ** (-log_probs.nanmean(dim=-1))
+            else:
+                raise ValueError("Invalid base type. Choose from 'exp' or float or int.")
+            
