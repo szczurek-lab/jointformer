9839580d4fdba8fe17920276edf8c27a0b95a802
AdamIzdebski <adam.izdebski1@gmail.com>
2024-09-06 16:17:46 +0200

    allow flash not availbale

--- jointformer/models/layers/attention.py
+++ jointformer/models/layers/attention.py
@@ -19,6 +19,11 @@ class Attention(nn.Module):
         self.bias = bias
         self.dropout = dropout
         self.block_size = block_size
+        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
+        if not self.flash:
+            self.register_buffer(
+                "causal_mask", torch.tril(torch.ones(self.block_size, self.block_size)).view(1, 1, self.block_size, self.block_size)
+                )
         
         # key, query, value projections for all heads, but in a batch
         self.qkv = nn.Linear(self.embedding_dim, 3 * self.embedding_dim, bias=bias)
@@ -58,10 +63,35 @@ class Attention(nn.Module):
         if attn_mask is not None:
             attn_mask = attn_mask.unsqueeze(1).unsqueeze(1).expand(batch_size, self.num_heads, seq_len, seq_len)
         
-        y = F.scaled_dot_product_attention(q, k, v, attn_mask=attn_mask, 
-            is_causal=is_causal, dropout_p=self.dropout if self.training else 0.)
-        
+        if self.flash:
+            y = F.scaled_dot_product_attention(q, k, v, attn_mask=attn_mask, 
+                is_causal=is_causal, dropout_p=self.dropout if self.training else 0.)
+        else:
+            y = self.scaled_dot_product_attention(q, k, v, attn_mask=attn_mask, 
+                is_causal=is_causal, dropout_p=self.dropout if self.training else 0.)
+            
         y = y.transpose(1, 2).contiguous().view(batch_size, seq_len, self.embedding_dim)        
         y = self.out(y)
         
         return y
+    
+    @staticmethod
+    def scaled_dot_product_attention(query, key, value, attn_mask, is_causal, dropout_p) -> torch.Tensor:
+        L, S = query.size(-2), key.size(-2)
+        scale_factor = 1 / math.sqrt(key.size(-1))
+        attn_bias = torch.zeros(L, S, dtype=query.dtype).cuda()
+        if is_causal:
+            assert attn_mask is None
+            temp_mask = torch.ones(L, S, dtype=torch.bool).tril(diagonal=0).cuda()
+            attn_bias.masked_fill_(temp_mask.logical_not(), float("-inf"))
+            attn_bias.to(query.dtype)
+        attn_weight = query @ key.transpose(-2, -1) * scale_factor
+        if attn_mask is not None:
+            if attn_mask.dtype == torch.bool:
+                attn_weight.masked_fill_(attn_mask==0, float("-inf"))
+            else:
+                attn_bias = attn_bias + attn_mask
+        attn_weight = torch.softmax(attn_weight, dim=-1)
+        attn_weight = torch.dropout(attn_weight, dropout_p, train=True)
+        return attn_weight @ value
+    
