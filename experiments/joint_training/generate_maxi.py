e8ebdcd1c0a3fd17d6d59b76023c81fa55a30c8a
Maximilian Armuss <maximilian-armuss-dev@proton.me>
2024-09-18 14:29:44 +0200

    delete encoder from geenerate script, update trianing config

--- experiments/joint_training/generate_maxi.py
+++ experiments/joint_training/generate_maxi.py
@@ -11,7 +11,9 @@ from jointformer.configs.tokenizer import TokenizerConfig
 from jointformer.configs.model import ModelConfig
 
 from jointformer.utils.tokenizers.auto import AutoTokenizer
+
 from jointformer.models.auto import AutoModel
+from jointformer.models.jointformer import Jointformer
 
 
 REPOSITORY_DIR = "/home/maxi/code/jointformer"
@@ -19,7 +21,7 @@ OUT_DIR_BASE = "/home/maxi/code/jf-data"
 os.chdir(REPOSITORY_DIR)
 
 PRETRAINED_JOINTFORMER_FILE = f"{OUT_DIR_BASE}/ckpt.pt"
-DATA_FILE = f"{OUT_DIR_BASE}/data/smiles.txt"
+DATA_FILE = f"{OUT_DIR_BASE}/data/guacamol/val/smiles.txt"
 OUTPUT_DIR = f"{OUT_DIR_BASE}/out-generate"
 
 PATH_TO_TOKENIZER_CONFIG = f"{REPOSITORY_DIR}/configs/tokenizers/smiles_separate_task_token"
@@ -33,22 +35,21 @@ tokenizer_config = TokenizerConfig.from_config_file(PATH_TO_TOKENIZER_CONFIG)
 tokenizer = AutoTokenizer.from_config(tokenizer_config)
 
 model_config = ModelConfig.from_config_file(PATH_TO_MODEL_CONFIG)
-model = AutoModel.from_config(model_config)
+model: Jointformer = AutoModel.from_config(model_config)
+
+# Cheating a bit here
+batch_size = 128
+model.update_batch_size(128)
 model.load_pretrained(PRETRAINED_JOINTFORMER_FILE)
+model.update_training_mode(False)
 
-encoding_batch_size = 16
-smiles_encoder = model.to_smiles_encoder(tokenizer=tokenizer, device=device, batch_size=encoding_batch_size)
-
-encoding = smiles_encoder.encode(smiles_data) 
-
-batch_size = 8
 temperature = 0.8
 top_k = 10
 
 generator = model.to_guacamole_generator(tokenizer=tokenizer, batch_size=batch_size, temperature=temperature, top_k=top_k, device=device)
-out_smiles = generator.generate(number_samples=16)
+out_smiles = generator.generate(number_samples=10)
 
-filename = f"gen-output_{time.strftime("%d_%m-%H_%M_%S")}"
+filename = f"gen-output_{time.strftime('%d_%m-%H_%M_%S')}"
 OUTFILE = os.path.join(OUTPUT_DIR, filename)
 open(OUTFILE, "w").writelines(out_smiles)
 print(f"Wrote {len(out_smiles)} lines to '{OUTFILE}'.")
